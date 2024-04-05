789f9536781415e752390acfe13a92c6321b944e
Omar Roman <omar.roman.2828@gmail.com>
2024-04-05 07:42:00 -0600

    quitar carpenas netbeans por bug xd

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fInicio = LocalDate.of(2024, 3, 1);
+        System.out.println("Fecha: "+fInicio.format(formato));
     }
 }
--- src/persistencia/entidad/Tarea.java
+++ src/persistencia/entidad/Tarea.java
@@ -2,6 +2,7 @@ package persistencia.entidad;
 
 import java.time.LocalDate;
 
+
 public class Tarea implements Almacenable {
     protected String codigo;
     protected Empleado responsable;
@@ -21,7 +22,7 @@ public class Tarea implements Almacenable {
     @Override
     public void fromStrCsv(String[] strCsvSplit) {
         this.codigo = strCsvSplit[0];
-        this.responsable = strCsvSplit[0];
+        //this.responsable = strCsvSplit[0];
         this.fInicio = LocalDate.parse(strCsvSplit[2]);
         this.fFin = LocalDate.parse(strCsvSplit[3]);
     }
